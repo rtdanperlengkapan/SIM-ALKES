9b9094332d33a3aa1db14cae4e6132d52aa6f32e
rtdanperlengkapan <divisiperlengkapanrsud.sekayu@gmail.com>
2026-06-19 10:00:00 +0700

    Add databaseAlkes array for medical equipment

---
+++ data.js
@@ -0,0 +1,8 @@
+// DATABASE ALKES RSUD SEKAYU
+const databaseAlkes = [
+    { id: 1, nama: "Ventilator Portable", ruangan: "ICU", tglPengadaan: "2022-03-15", lifetimeTahun: 5 },
+    { id: 2, nama: "Mesin EKG 12 Channel", ruangan: "Poli Jantung", tglPengadaan: "2019-08-20", lifetimeTahun: 7 },
+    { id: 3, nama: "Pasien Monitor", ruangan: "UGD", tglPengadaan: "2025-01-10", lifetimeTahun: 5 },
+    { id: 4, nama: "Defibrillator", ruangan: "ICU", tglPengadaan: "2017-05-12", lifetimeTahun: 8 },
+    { id: 5, nama: "Infuse Pump", ruangan: "Rawat Inap kelas 1", tglPengadaan: "2024-06-01", lifetimeTahun: 5 }
+];
